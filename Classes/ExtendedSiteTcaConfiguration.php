c881d0d2ff92c4a16e33eb1d5894dc7e421cd8eb
Lukas Niestroj <lukas.niestroj@werkraum.net>
2024-06-26 16:09:59 +0200

    overwrite SiteTcaConfiguration to unset TCA user type

---
+++ Classes/ExtendedSiteTcaConfiguration.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * This file is part of TYPO3 CMS-based extension "deepl_translate" by werkraum.
+ *
+ *  It is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License, either version 2
+ *  of the License, or any later version.
+ *
+ */
+
+/*
+ * This file is part of TYPO3 CMS-based extension "deepl_translate" by werkraum.
+ *
+ *  It is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License, either version 2
+ *  of the License, or any later version.
+ *
+ */
+
+namespace Werkraum\DeeplTranslate;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Configuration\SiteTcaConfiguration;
+use TYPO3\CMS\Backend\Routing\Route;
+
+/**
+ * Only to manage the unavailable TCA column type 'user' on save
+ */
+class ExtendedSiteTcaConfiguration extends SiteTcaConfiguration
+{
+
+    public function getTca(): array
+    {
+        $tca = parent::getTca();
+
+        /** @var ServerRequestInterface $request */
+        $request = $GLOBALS['TYPO3_REQUEST'];
+
+        /** @var Route $routing */
+        if ($route = $request->getAttribute('route')) {
+            if ( $route->getPath() === '/module/site/configuration/save' ) {
+                $tca['site']['columns']['deepl_split_content_by_selectors']['config']['type'] = 'text';
+                $tca['site']['columns']['deepl_recaptcha']['config']['type'] = 'text';
+                $tca['site']['columns']['deepl_recaptcha_redirect_to_page']['config']['type'] = 'text';
+            }
+        }
+
+        return $tca;
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,15 @@
  *
  */
 
+/*
+ * This file is part of TYPO3 CMS-based extension "deepl_translate" by werkraum.
+ *
+ *  It is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License, either version 2
+ *  of the License, or any later version.
+ *
+ */
+
 require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('wr_deepl_translate') . '/Resources/Private/Libs/vendor/autoload.php';
 
 //if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['wr_deepl_translate']['caching']['cacheConfigurations']['deepl_translate_cache'])) {
@@ -40,4 +49,8 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1717762840785] 
     'nodeName' => 'extendedVersion',
     'priority' => 40,
     'class' => \Werkraum\DeeplTranslate\Backend\Form\Element\ExtendedVersionElement::class,
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Configuration\SiteTcaConfiguration::class] = [
+    'className' => \Werkraum\DeeplTranslate\ExtendedSiteTcaConfiguration::class,
 ];
