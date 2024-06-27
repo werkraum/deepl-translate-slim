6e61beb344f9df8c648b40978383931b1452eddd
Lukas Niestroj <lukas.niestroj@werkraum.net>
2024-06-27 06:57:50 +0200

    release 1.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,11 +17,20 @@
  *
  */
 
+/*
+ * This file is part of TYPO3 CMS-based extension "deepl_translate" by werkraum.
+ *
+ *  It is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License, either version 2
+ *  of the License, or any later version.
+ *
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'DeepL Translate Slim',
     'description' => 'Translate TYPO3 pages with DeepL without headaches',
     'category' => 'plugin',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'author' => 'Lukas Niestroj',
     'author_email' => 'lukas.niestroj@werkraum.net',
     'author_company' => 'werkraum.net',
