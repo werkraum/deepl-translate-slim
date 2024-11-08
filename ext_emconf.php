e206d2c55128c12f7b5c0a22b9191f6496fc0495
Lukas Niestroj <lukas.niestroj@werkraum.net>
2024-11-08 10:48:13 +0100

    update version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,14 +12,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'DeepL Translate Slim',
     'description' => 'Automatic translations of entire TYPO3 websites with DeepL. Quick and easy. Without extra languages in the backend and without headaches.',
     'category' => 'plugin',
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'author' => 'Lukas Niestroj',
     'author_email' => 'lukas.niestroj@werkraum.net',
     'author_company' => 'werkraum.net',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
